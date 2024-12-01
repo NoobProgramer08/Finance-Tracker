6ed8409f7275f14033fa997fafdd18a50a96b084
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2024-12-02 00:15:35 +0800

    Finalized the architecture

--- Architecture/Scripts/LandingPage/script.js
+++
@@ -1,8 +0,0 @@
-const getStarted = document.querySelector("#getStarted");
-
-getStarted.addEventListener("click", transferToForm);
-
-function transferToForm() {
-  window.location.pathname =
-    "/Habit-Tracker-System/Architecture/app/Form/index.html";
-}
