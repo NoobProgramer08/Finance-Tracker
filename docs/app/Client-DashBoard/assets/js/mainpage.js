04a28d3abe16ad6fab7742e0e237486bb5f9bc4b
Windyl Monton <windyl1.monton.lvhs@gmail.com>
2025-01-02 00:52:19 +0800

    Created the ui for client dashboard

---
+++ docs/app/Client-DashBoard/assets/js/mainpage.js
@@ -0,0 +1,21 @@
+const dropdownButton  = document.querySelector("#dropdown-trigger");
+const dropDowns = document.querySelector("#dropdowns")
+
+dropdownButton.addEventListener("click",displayDropdown);
+
+
+
+function displayDropdown(e){
+    e.preventDefault();
+    const dropDownValue = "Log out";
+    const link = document.createElement("input");
+    link.value = dropDownValue;
+    
+    dropDowns.appendChild();
+    
+
+
+
+
+
+}
